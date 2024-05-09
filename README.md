9f0632a114bb73ed3445fd17568da70cc9afaa11
Taher Asghar <taherasghar59@gmail.com>
2024-05-09 13:00:41 +0300

    Finished task 2

---
+++ README.md
@@ -0,0 +1,3 @@
+# Prodigy_WD_02
+This project is my second task in Prodigy Infotech internship, to solidify my html,css,and js knowledge
+It is a stop watch with nice css green theme design and interactive buttons.
---
+++ app.js
@@ -0,0 +1,44 @@
+var min         = 0;
+var sec         = 0;
+var msec        = 0;
+var minHeading  = document.getElementById('min');
+var secHeading  = document.getElementById('sec');
+var msecHeading = document.getElementById('msec');
+var interval;
+var btn = document.getElementById('start')
+
+
+function timer(){
+    msec++
+    msecHeading.innerHTML = msec;
+    if(msec >= 100){
+        sec++
+        secHeading.innerHTML = sec;
+        msec = 0;
+    }
+    if(sec >= 60){
+        min ++
+        minHeading.innerHTML = min;
+        sec = 0;
+    }
+}
+
+function startBtn(){
+    interval = setInterval(timer, 10);
+    btn.disabled = true;
+}
+
+function stopBtn(){
+    clearInterval(interval)
+    btn.disabled = false;
+}
+
+function resetBtn(){
+    min  = 0;
+    sec  = 0;
+    msec = 0;
+    minHeading.innerHTML  = min;
+    secHeading.innerHTML  = sec;
+    msecHeading.innerHTML = msec;
+    stopBtn();
+}
